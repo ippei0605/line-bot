6393946695096fc9a4ce2d9bc2661f4c27463340
ippei0605 <ippei0605@gmail.com>
2016-09-20 17:58:13 +0900

    ・サービス名を本番環境に合わせて変更。 ・各環境のサービス名統一が難しいので接続情報をVCAP_SERVICESの配列順で取得するように変更。

--- utils/context.js
+++ utils/context.js
@@ -5,12 +5,12 @@
  * @author Ippei SUZUKI
  */
 
-var STATICA_SERVICE_NAME = 'statica-service';
-var VISUAL_RECOGNITION_SERVICE_NAME = 'visual-recognition-service';
-
 // 環境変数を取得する。
 var cfenv = require('cfenv');
 var appEnv = cfenv.getAppEnv();
+
+// VCAP_SERVICES
+var vcapServices = JSON.parse(process.env.VCAP_SERVICES);
 
 /** 環境変数 */
 exports.appEnv = appEnv;
@@ -25,31 +25,30 @@ exports.path = require('path');
 exports.request = require('request');
 
 /** STATICA URL */
-var staticaCreds = appEnv.getServiceCreds(STATICA_SERVICE_NAME);
+var staticaCreds = vcapServices.statica[0].credentials;
 exports.staticaUrl = staticaCreds.STATICA_URL;
 
 /** Watson Visual Recognition */
-// ref https://github.com/watson-developer-cloud/node-sdk
 var watson = require('watson-developer-cloud');
-var visualRecognitionCreds = appEnv.getServiceCreds(VISUAL_RECOGNITION_SERVICE_NAME);
+var visualRecognitionCreds = vcapServices.watson_vision_combined[0].credentials;
 var visualRecognition = watson.visual_recognition({
-    api_key: visualRecognitionCreds.api_key,
-    version: 'v3',
-    version_date: '2016-05-19'
+    "api_key": visualRecognitionCreds.api_key,
+    "version": "v3",
+    "version_date": "2016-05-19"
 });
 exports.visualRecognition = visualRecognition;
 
 /** LINE BOT API Header */
 exports.headers = {
-    'Content-Type': 'application/json; charset=UTF-8',
-    'X-Line-ChannelID': process.env.CHANNEL_ID,
-    'X-Line-ChannelSecret': process.env.CHANNEL_SECRET,
-    'X-Line-Trusted-User-With-ACL': process.env.MID
+    "Content-Type": "application/json; charset=UTF-8",
+    "X-Line-ChannelID": process.env.CHANNEL_ID,
+    "X-Line-ChannelSecret": process.env.CHANNEL_SECRET,
+    "X-Line-Trusted-User-With-ACL": process.env.MID
 };
 
 /**
  * classify, detectFaces
  */
 exports.appSetting = {
-    recognizeMode : 'detectFaces'
-}
+    "recognizeMode": "detectFaces"
+};
